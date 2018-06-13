78b3fa8a26b774fdd664e4daa9eeadf3e2bae45c
Apollo <Apollo@Apollo.attlocal.net>
2018-06-13 02:11:51 -0700

    update styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Card, CardTitle, CardText, Paper } from 'react-md';
+import { NavigationDrawer } from 'react-md';
 
 const style = { maxWidth: 320 };
 
@@ -10,20 +11,31 @@ class App extends Component {
 
   render() {
     return <div className="App">
-    <Paper
-    zDepth={3}
-    > 
-        <Card style={style} className="md-block-centered">
+				<NavigationDrawer drawerTitle="react-md with CRA" toolbarTitle="Welcome to NY TIMES SEARCH">
+        <Paper zDepth={0}>
+        	<div className="App"
+               zDepth={3}
+               >
+            <header className="App-header">
+							<img src={logo} className="App-logo" alt="logo" />
+							<h1 className="App-title">Powered by React</h1>
+						</header>
+					</div>
+			  </Paper>
+        <Paper zDepth={5}>
+          <Card style={style} className="md-block-centered">
+          <Paper zDepth={3}>
             <CardTitle title="Using CardTitle" subtitle="With CardText" />
             <CardText>
               <p>
-                The <code>CardText</code> component is really just useful for displaying any
-                content with some additional padding.
-          </p>
-            </CardText>  
-        </Card>
-      </Paper>     
-			</div>;
+                The <code>CardText</code> component is really just useful for displaying any content with some additional padding.
+              </p>
+            </CardText>
+          </Paper>  
+          </Card>
+        </Paper>  
+      </NavigationDrawer>
+			</div>;    
   }
 }
 
