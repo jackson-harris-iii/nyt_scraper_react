b0e71ba46f6c89a61e5f50798b0429d4d1cbe9e0
Apollo <Apollo@Apollo.local>
2018-06-23 01:36:38 -0700

    update api util

--- Client/src/utils/API.js
+++ Client/src/utils/API.js
@@ -2,8 +2,13 @@ import axios from "axios";
 
 export default {
     
-    search: (query) => {
-        return axios.post("/api/articles", query)
+    search: (topic, startYear, endYear) => {
+        
+        const authKey = "cfb9f6bf533b46d0a9d4edf5059f55b9";
+        const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+            authKey + "&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "0101";
+
+        return axios.get(queryURL);        
     },
 
     savedArticles: () => {
